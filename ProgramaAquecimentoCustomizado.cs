f451f437f821c987c175ad59a64dfe81a41d3acb
Pedro Dantas <140216375+PedroDantasDev@users.noreply.github.com>
2023-07-21 20:45:45 -0300

    Create ProgramaAquecimentoCustomizado.cs

---
+++ ProgramaAquecimentoCustomizado.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoMicroondasDigital
+{
+    public class ProgramaAquecimentoCustomizado
+    {
+        public string Nome { get; set; }
+        public string Alimento { get; set; }
+        public int Tempo { get; set; }
+        public int Potencia { get; set; }
+        public string CaractereAquecimento { get; set; }
+        public string Instrucoes { get; set; }
+
+        public ProgramaAquecimentoCustomizado(string nome, string alimento, int tempo, int potencia, string caractereAquecimento, string instrucoes)
+        {
+            Nome = nome;
+            Alimento = alimento;
+            Tempo = tempo;
+            Potencia = potencia;
+            CaractereAquecimento = caractereAquecimento;
+            Instrucoes = instrucoes;
+        }
+    }
+}
